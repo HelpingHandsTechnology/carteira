a41e87019f6c1c05601fc0454fc2162e0eb45290
Luis Felipe Sena <luisfelipesena@gmail.com>
2025-02-13 15:35:09 -0300

    experimental__runtimeEnv

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,9 +1,5 @@
-import { loadEnvConfig } from '@next/env';
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
-
-const projectDir = process.cwd();
-loadEnvConfig(projectDir);
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
@@ -12,9 +8,7 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_API_URL: z.string().url().optional(),
   },
-  runtimeEnv: {
+  experimental__runtimeEnv: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || process.env.URL,
-    DATABASE_URL: process.env.DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV,
   },
 });
