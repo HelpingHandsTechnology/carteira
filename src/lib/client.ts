a66a6b7a9de52458937f5a00ca20475f56d050fd
Luis Felipe Sena <luisfelipesena@gmail.com>
2025-02-13 01:57:05 -0300

    use @t3-oss/env-nextjs

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,5 +2,5 @@ import { AppType } from "../server/app"
 import { hc } from "hono/client"
 import { env } from "./env"
 
-export const client = hc<AppType>(env.NEXT_PUBLIC_URL ?? '')
+export const client = hc<AppType>(env.NEXT_PUBLIC_API_URL ?? '')
 console.log({ClientEnv: env})
