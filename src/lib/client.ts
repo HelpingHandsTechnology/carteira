21337c169a5f1ee158a3452abfd0b96524cfe908
Luis Felipe Sena <luisfelipesena@gmail.com>
2025-02-13 00:26:18 -0300

    a

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,4 +2,4 @@ import { AppType } from "../server/app"
 import { hc } from "hono/client"
 import { env } from "./env"
 
-export const client = hc<AppType>(env.URL)
+export const client = hc<AppType>(env.PUBLIC_URL)
