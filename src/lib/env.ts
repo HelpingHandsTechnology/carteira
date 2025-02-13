01cb45b9df421c25975fff0201e1c22a4cace37a
Luis Felipe Sena <luisfelipesena@gmail.com>
2025-02-13 02:00:48 -0300

    fix

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,7 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
- 
+
+console.log({ envs: process.env });
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
@@ -10,7 +11,7 @@ export const env = createEnv({
     NEXT_PUBLIC_API_URL: z.string().url(),
   },
   runtimeEnv: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || process.env.URL,
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
   },
