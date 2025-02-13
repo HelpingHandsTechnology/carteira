629a299ef6c99d4433fc1adb554d010912caddd9
Luis Felipe Sena <luisfelipesena@gmail.com>
2025-02-13 00:14:53 -0300

    trigger postman

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,9 +1,8 @@
 import { z } from "zod"
 
 const _env = z.object({
-  // DATABASE_URL: z.string().url(),
+  DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "production", "test"]),
 })
-
 
 export const env = _env.parse(process.env)
