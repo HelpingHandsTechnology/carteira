502bae5568339e78cd48e87f034388e993b14991
Luis Felipe Sena <luisfelipesena@gmail.com>
2025-02-13 00:08:44 -0300

    prod

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 
 const _env = z.object({
-  DATABASE_URL: z.string().url(),
+  // DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "production", "test"]),
 })
 
